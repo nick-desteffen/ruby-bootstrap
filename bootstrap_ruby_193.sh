8c0ed2e76d881d0d7c527ed66527b891f51a362f
Nick DeSteffen <nick.desteffen@gmail.com>
2012-07-25 14:10:11 -0500

    Adding libxslt-dev libxml2-dev to package install list

--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt-dev libxml2-dev
 cd /tmp
 wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
 tar xzvf ruby-1.9.3-p194.tar.gz
