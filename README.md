9412abf555d23296f2fd6ce3ff03f5db607a9354
Nick DeSteffen <nick.desteffen@gmail.com>
2012-10-14 21:24:24 -0500

    Updating to ruby 1.9.3-p286

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Ruby Bootstrap
 ==============
 
-Bash script to bootstrap an Ubuntu 12.04 install with Ruby 1.9.3-p194
+Bash script to bootstrap an Ubuntu 12.04 install with Ruby 1.9.3-p286
 
-To bootstrap 1.9.3-p194:  
+To bootstrap 1.9.3-p286:  
 `curl -L https://raw.github.com/nick-desteffen/ruby-bootstrap/master/bootstrap_ruby_193.sh | sudo bash`
--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev ncurses-dev
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
-tar xzvf ruby-1.9.3-p194.tar.gz
-cd ruby-1.9.3-p194/
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p286.tar.gz
+tar xzvf ruby-1.9.3-p286.tar.gz
+cd ruby-1.9.3-p286/
 ./configure --prefix=/usr/local
 make
 make install
