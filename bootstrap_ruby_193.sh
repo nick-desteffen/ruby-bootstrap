4df80027fb759c136353f7df6dbf2de642e0ba64
Nick DeSteffen <nick.desteffen@gmail.com>
2012-11-12 22:10:59 -0600

    Updating to ruby 1.9.3-p327

--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev ncurses-dev
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p286.tar.gz
-tar xzvf ruby-1.9.3-p286.tar.gz
-cd ruby-1.9.3-p286/
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
+tar xzvf ruby-1.9.3-p327.tar.gz
+cd ruby-1.9.3-p327/
 ./configure --prefix=/usr/local
 make
 make install
