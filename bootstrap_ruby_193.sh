09868e51d2b78fe4039014c93bb13ea481500fa6
Nick DeSteffen <nick.desteffen@gmail.com>
2012-12-12 21:44:50 -0600

    Updating package names for Ubuntu 12.10

--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev ncurses-dev
+apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
 wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
 tar xzvf ruby-1.9.3-p327.tar.gz
