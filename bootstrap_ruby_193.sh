d43024120bba933a3689a46fc53fdc6d3c9d9e43
Nick DeSteffen <nick.desteffen@gmail.com>
2013-01-22 08:16:40 -0600

    Updating to patchlevel 374

--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p362.tar.gz
-tar xzvf ruby-1.9.3-p362.tar.gz
-cd ruby-1.9.3-p362/
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p374.tar.gz
+tar xzvf ruby-1.9.3-p374.tar.gz
+cd ruby-1.9.3-p374/
 ./configure --prefix=/usr/local
 make
 make install
