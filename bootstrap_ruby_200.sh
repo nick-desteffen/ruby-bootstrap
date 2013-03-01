6bbe85485c46d1927078b1ad988d70c75c6efb62
Nick DeSteffen <nick.desteffen@gmail.com>
2013-02-28 21:09:27 -0600

    Script to bootstrap ruby 2.0

---
+++ bootstrap_ruby_200.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+apt-get -y update
+apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
+cd /tmp
+wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p0.tar.gz
+tar xzvf ruby-2.0.0-p0.tar.gz
+cd ruby-2.0.0-p0/
+./configure --prefix=/usr/local
+make
+make install
+gem update --system
+gem install bundler
+ruby -v
