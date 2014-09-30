f36879e95758dba9ac0b723360bd8f75d3fd30fe
Nick DeSteffen <nick.desteffen@gmail.com>
2014-09-29 19:06:34 -0500

    Update to latest releases

--- bootstrap_ruby_210.sh
+++ bootstrap_ruby_210.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
-tar xzvf ruby-2.1.2.tar.gz
-cd ruby-2.1.2/
+wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.3.tar.gz
+tar xzvf ruby-2.1.3.tar.gz
+cd ruby-2.1.3/
 ./configure --prefix=/usr/local
 make
 make install
