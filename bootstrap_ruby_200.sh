96f953c2a1448196677c06140f87abdeb7c82a3a
Nick DeSteffen <nick.desteffen@gmail.com>
2014-11-03 21:52:16 -0600

    Latest rubyies

--- bootstrap_ruby_200.sh
+++ bootstrap_ruby_200.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p576.tar.gz
-tar xzvf ruby-2.0.0-p576.tar.gz
-cd ruby-2.0.0-p576/
+wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p594.tar.gz
+tar xzvf ruby-2.0.0-p594.tar.gz
+cd ruby-2.0.0-p594/
 ./configure --prefix=/usr/local
 make
 make install
