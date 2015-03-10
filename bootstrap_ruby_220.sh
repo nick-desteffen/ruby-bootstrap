5623aff783bfb8b21ec72074f7c953f87aebaa12
Nick DeSteffen <nick.desteffen@gmail.com>
2015-03-09 19:32:04 -0500

    Updating rubies

--- bootstrap_ruby_220.sh
+++ bootstrap_ruby_220.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz
-tar xzvf ruby-2.2.0.tar.gz
-cd ruby-2.2.0/
+wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.1.tar.gz
+tar xzvf ruby-2.2.1.tar.gz
+cd ruby-2.2.1/
 ./configure --prefix=/usr/local
 make
 make install
